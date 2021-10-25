ee66712f57324eaa4dee28aed59ea4b51d6d7403
王云鹏 <yunpeng.wang@weimob.com>
2021-10-25 19:59:45 +0800

    +++

---
+++ packages/demo/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-button>测试</el-button>
+</template>
+
+<script>
+import { Button } from "element-ui";
+export default {
+  components: {
+    "el-button": Button,
+  },
+  data: () => {},
+};
+</script>
+
+<style lang="scss" scoped></style>
